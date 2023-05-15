d0f37130d6e9486d79664e6d17f84e703bb5d3d7
alecksandr <sansepiol26@gmail.com>
2023-05-15 16:57:39 -0600

    Working on the setup and starting building a command line interface

--- setup.py
+++ setup.py
@@ -1,4 +1,34 @@
+from setuptools import setup, find_packages
 
+# To make the project pip installable
+setup(
+    name="sld",  # Sign Language Detection
+    version="1.0.0",
+    description="Sign Language Gesture Recognition project: Python, OpenCV, Mediapipe, and Random Forest algorithm "
+                "combine to capture, preprocess, and classify hand landmarks for accurate real-time sign language "
+                "gesture recognition.",
+    author="Erick Carrillo, Raudel Casas, Andres Peña",
+    author_email="sansepiol26@gmail.com",
+    url="https://github.com/RaudelCasas1603/Monky-Detection-",  # Update with the correct URL
 
+    packages=find_packages(exclude=["test"]),
+    install_requires=[
+        "opencv-python",
+        "mediapipe",
+        "scikit-learn",
+        # Add other dependencies here
+    ],
+    
+    # Add the features to here
+    entry_points={
+        "console_scripts": [
+            "sld = src.__main__:main",
+        ]
+    },
 
-
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
